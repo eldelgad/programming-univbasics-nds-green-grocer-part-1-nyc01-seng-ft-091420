b10ef5255b419e3bb83cfbb6da7040b33c227a1f
Elias Delgado <evdelgado99@gmail.com>
2020-09-10 19:29:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,6 +18,7 @@ def consolidate_cart(cart)
   while counter < cart.length
 
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    binding.pry
   
     if new_cart_item != nil
       new_cart_item[:count] += 1
