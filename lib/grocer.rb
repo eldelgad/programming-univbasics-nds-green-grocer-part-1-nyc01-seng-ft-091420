d569dee3682568a9c2ee1331f9b7de85622f05e1
Elias Delgado <evdelgado99@gmail.com>
2020-09-10 19:07:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -40,33 +40,33 @@ end
 # method consolidate_cart takes in the argument cart (AoHoItems)
   # return a new_cart (AoH) with each item containing a quantity(possible index manipualtion)
 
-# def consolidate_cart(cart)
+def consolidate_cart(cart)
   
-#   # REMEMBER: This returns a new Array that represents the cart. Don't merely
-#   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  # REMEMBER: This returns a new Array that represents the cart. Don't merely
+  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-# new_cart = []
-# counter = 0
+new_cart = []
+counter = 0
 
-#   while counter < cart.length
+  while counter < cart.length
 
-#     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
   
-#     if new_cart_item != nil
-#       new_cart_item[:count] += 1
-#     else
-#       new_cart_item = {
-#         :item => cart[counter][:item],
-#         :price => cart[counter][:price],
-#         :clearance => cart[counter][:clearance],
-#         :count => 1
-#       }
+    if new_cart_item != nil
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
+        :item => cart[counter][:item],
+        :price => cart[counter][:price],
+        :clearance => cart[counter][:clearance],
+        :count => 1
+      }
       
-#       new_cart << new_cart_item
+      new_cart << new_cart_item
   
-#     end
-#     counter += 1
-#   end
-#   new_cart
-# end
+    end
+    counter += 1
+  end
+  new_cart
+end
 
