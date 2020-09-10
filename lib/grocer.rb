4fb17b6ba1e493ad40d3b6c419c9a4a238a58c44
Elias Delgado <evdelgado99@gmail.com>
2020-09-10 19:13:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-####### PSEUDOCODE #######
-
-# method find_item_by_name_in_collection takes the name, collection arguments
-  # should iterate over the collection.each do  |key, value| 
-    # check the value of the key (:item) in each nested hash
-  # if the value is equal to the name the method should return that entire nested hash
-    # re-iterate through the collection & hash
-  # if an :item is not found, find_item_by_name_in_collection should return nil
-
 def find_item_by_name_in_collection(name, collection)
   counter = 1
   while counter < collection.length
@@ -17,34 +8,6 @@ def find_item_by_name_in_collection(name, collection)
   end
   
 end
-  
-  # collection.each do |hash|
-    
-  #   hash.each do |key, value|
-  #     return hash if value == name
-  #   end
-    
-  # end
-  
-  # collection.each do |hash|
-    
-  #   hash.each do |key, value|
-  #     return nil if value != name
-  #   end
-    
-  # end
-
-# end
-
-####### PSEUDOCODE #######
-
-  
-# REMEMBER: This returns a new Array that represents the cart. Don't merely
-# change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-
-# method consolidate_cart takes in the argument cart (AoHoItems)
-  # return a new_cart (AoH) with each item containing a quantity(possible index manipualtion)
 
 def consolidate_cart(cart)
 
